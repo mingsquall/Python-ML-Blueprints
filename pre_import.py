4bf5b2515a1417b2eff3c546fec845b5d3c55161
YuanMing Liu <774166816@qq.com>
2017-08-25 12:26:00 +0800

    add pre_import file

---
+++ pre_import.py
@@ -0,0 +1,14 @@
+# 在这里做一些准备工作 :) 然后在Jupyter中使用 from pre_import import * 就可以愉快的py了
+import os
+import pandas as pd
+import requests
+
+# 获取数据：使用Requests下载数据集iris.data
+PATH = r'/Users/patientman/Desktop/irisi'
+r = requests.get('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data')
+# 准备数据文件
+with open(PATH + 'iris.data', 'w') as f:
+    f.write(r.text)
+os.chdir(PATH)
+# 读取CSV数据文件，并增加列名，生成DataFrame
+df = pd.read_csv(PATH + 'iris.data', names=['sepal length', 'sepal width', 'petal length', 'petal width', 'class'])
