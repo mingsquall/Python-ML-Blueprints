f6bebd17708da94e0a770f9ab1920b9faf34cb63
YuanMing Liu <774166816@qq.com>
2017-08-25 18:44:23 +0800

    add matplotlib&seaborn-demo

--- pre_import.py
+++ pre_import.py
@@ -1,10 +1,15 @@
 # 在这里做一些准备工作 :) 然后在Jupyter中使用 from pre_import import * 就可以愉快的py了
+# -*- coding: utf-8 -*-
 import os
 import pandas as pd
 import requests
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.style.use('ggplot')
 
 # 获取数据：使用Requests下载数据集iris.data
-PATH = r'/Users/patientman/Desktop/irisi'
+PATH = r'/Users/patientman/Desktop/iris'
 r = requests.get('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data')
 # 准备数据文件
 with open(PATH + 'iris.data', 'w') as f:
@@ -12,3 +17,5 @@ with open(PATH + 'iris.data', 'w') as f:
 os.chdir(PATH)
 # 读取CSV数据文件，并增加列名，生成DataFrame
 df = pd.read_csv(PATH + 'iris.data', names=['sepal length', 'sepal width', 'petal length', 'petal width', 'class'])
+
+import seaborn as sns
